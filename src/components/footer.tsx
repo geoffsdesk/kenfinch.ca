5e97cf99f400685f6b2525c9f0c61a404752bb25
Geoffrey Anderson <geoff.radian6@gmail.com>
2025-07-26 03:32:34 +0000

    there is some type of icon to the left of the facebook social icon pleas

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,9 +15,6 @@ export function Footer() {
             <Mail className="h-4 w-4" />
             ken@kenfinch.ca
           </a>
-          <Link href="#" className="text-muted-foreground hover:text-foreground">
-            <TwitterIcon className="h-5 w-5" />
-          </Link>
           <Link href="https://www.facebook.com/KenFinchRealEstate/" className="text-muted-foreground hover:text-foreground">
             <FacebookIcon className="h-5 w-5" />
           </Link>
@@ -51,25 +48,6 @@ function RealtorIcon(props: React.SVGProps<SVGSVGElement>) {
             <polyline points="9 22 9 12 15 12 15 22" />
         </svg>
     );
-}
-
-function TwitterIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 1.4 3.3 4.9 3 7.1 0 .8-1.7 1.5-3.3 1.5-1.3 0-2.8-.5-4.2-1.3-1.4-.8-2.8-1.9-4.2-3.1-1.4-1.2-2.8-2.5-4.2-4C2 6.5 4 4 4 4s2.2 1.3 3.5 2.2c1.3.9 2.8 1.9 4.2 2.8 1.4.9 2.8 1.6 4.2 2.1 1.4.5 2.8.8 4.2 1 .7 0 1.2-.2 1.5-.5.3-.3.5-.7.5-1.2 0-.5-.2-1-.5-1.5s-.7-1-1.2-1.5c-.5-.5-1-1-1.5-1.5s-1-1-1.5-1.5c-.5-.5-1-1-1.5-1.5s-1-1-1.5-1.5z" />
-    </svg>
-  );
 }
 
 function FacebookIcon(props: React.SVGProps<SVGSVGElement>) {
