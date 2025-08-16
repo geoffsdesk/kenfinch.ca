d847f42746a2b2970ca45b30e83c4e4a4a69b36d
Geoffrey Anderson <geoff.radian6@gmail.com>
2025-08-16 17:57:35 +0000

    update the contact form image to be the sold_by_ken1.png image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -193,16 +193,16 @@ export default function Home() {
         <section id="contact" className="w-full py-8 md:py-12 lg:py-16">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-8 text-center">
-                <div className="mx-auto max-w-5xl">
+                <div className="mx-auto max-w-3xl">
                     <img
-                        src="/sold_by.png"
+                        src="/sold_by_ken1.png"
                         width="1200"
                         height="400"
                         alt="A sign showing a house sold by Ken Finch"
-                        className="mx-auto aspect-[3/1] overflow-hidden rounded-xl object-cover"
+                        className="mx-auto aspect-auto overflow-hidden rounded-xl object-cover"
                     />
                 </div>
-              <div className="space-y-2">
+              <div className="space-y-2 pt-8">
                 <h2 className="font-headline text-3xl font-bold tracking-tighter sm:text-5xl">Contact Ken</h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   Have questions or ready to get started? Reach out today.
